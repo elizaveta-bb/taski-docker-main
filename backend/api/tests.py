b0e1cf7268dc2deea29e67fc8ca43fb6c7db845a
Elizaveta Babenkova <liz2405@mail.ru>
2025-10-09 17:02:58 +0300

    add tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,11 +1,14 @@
-# backend/api/tests.py
 from http import HTTPStatus
 
 from api import models
 from django.test import Client, TestCase
 
+
 class TaskiAPITestCase(TestCase):
+    """Тесты для проверки работы API задач."""
+
     def setUp(self):
+        """Создание тестового клиента."""
         self.guest_client = Client()
 
     def test_list_exists(self):
